e498749bfc79274a409977cddcc8397fb2840e2f
unknown <gundambison@gmail.com>
2023-03-14 23:25:47 +0700

    customer add data

--- amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class CustomerApplication {
     public static void main(String[] args) {
+
         SpringApplication.run( CustomerApplication.class, args);
     }
 }
--- amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -3,8 +3,7 @@ package com.amigoscode.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
-
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request)
     {
         Customer customer = Customer.builder()
@@ -13,5 +12,9 @@ public record CustomerService() {
                 .email(request.email())
                 .build();
         //todo: check is valid/not
+
+        //save data
+        customerRepository.save(customer);
+        System.out.println("save has done");
     }
 }
