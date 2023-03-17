d027bd25ad0260eae197babc8ae56fbdd2982675
Gunawan <gundambison@gmail.com>
2023-03-17 23:53:14 +0700

    Rest Template

--- amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,11 +2,13 @@ package com.amigoscode.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService  {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request)
     {
         Customer customer = Customer.builder()
@@ -15,9 +17,21 @@ public class CustomerService  {
                 .email(request.email())
                 .build();
         //todo: check is valid/not
-
+        //todo: email is not taken
         //save data
-        customerRepository.save(customer);
-        System.out.println("save has done");
+        customerRepository.saveAndFlush(customer);
+        //todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+            "http://localhost:8081/api/v1/fraud-check/{customerId}",
+            FraudCheckResponse.class,
+            customer.getId()
+        );
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraudster");
+        }
+        //todo: send notification
+
+        //System.out.println("save has done");
     }
 }
