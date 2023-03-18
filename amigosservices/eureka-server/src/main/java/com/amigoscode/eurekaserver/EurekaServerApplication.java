8d9a16241c473afb4f1a4b5210c72887f8b55dc3
Gunawan <gundambison@gmail.com>
2023-03-18 22:18:25 +0700

    eureka clients

--- amigosservices/eureka-server/src/main/java/com/amigoscode/eurekaserver/EurekaServerApplication.java
+++ amigosservices/eureka-server/src/main/java/com/amigoscode/eurekaserver/EurekaServerApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
 @SpringBootApplication
 @EnableEurekaServer
-
+//todo: something
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run( EurekaServerApplication.class, args );
